3ad4d3e48ddeb326adf381c3a0a1963f8aa8de9f
Laszlo Csele <laszlo.csele90@gmail.com>
2019-03-02 20:16:20 +0100

    fix(location): get locations for current day

--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -6,7 +6,6 @@ import { BehaviorSubject } from 'rxjs';
 
 @Injectable()
 export class FirebaseService {
-  selfId: string;
   firestore: firebase.firestore.Firestore;
   visitorsCollection: firebase.firestore.CollectionReference;
   locations$ = new BehaviorSubject([]);
@@ -14,33 +13,28 @@ export class FirebaseService {
     this.initApp();
     this.firestore = firebase.firestore();
     this.visitorsCollection = this.firestore.collection('visitors');
-
-    window.onbeforeunload = () => {
-      this.removeLocation(this.selfId);
-      setTimeout(() => {
-        console.log('hey');
-      }, 2000);
-    };
   }
 
   addLocation(coords: Coordinates) {
-    this.visitorsCollection
-      .add({
-        altitude: coords.altitude,
-        longitude: coords.longitude,
-        latitude: coords.latitude,
-        accuracy: coords.accuracy,
-      })
-      .then(res => {
-        this.selfId = res.id;
-      });
+    this.visitorsCollection.add({
+      altitude: coords.altitude,
+      longitude: coords.longitude,
+      latitude: coords.latitude,
+      accuracy: coords.accuracy,
+      date: new Date(),
+    });
   }
   removeLocation(id: string) {
     this.visitorsCollection.doc(id).delete();
   }
 
   getLocations() {
-    this.visitorsCollection.onSnapshot(snapshot => {
+    const date = new Date();
+    date.setHours(0);
+    date.setMinutes(0);
+    date.setSeconds(0);
+    date.setMilliseconds(0);
+    this.visitorsCollection.where('date', '>=', date).onSnapshot(snapshot => {
       const docs = snapshot.docs.map(d => ({ id: d.id, ...d.data() }));
       this.locations$.next(docs);
     });
